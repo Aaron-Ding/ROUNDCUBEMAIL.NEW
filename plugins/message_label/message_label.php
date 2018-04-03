25dc884daaf1bf1a6ebe455911b1e79e8ed5eecc
qingyang <qingyang@sinoramagroup.com>
2018-04-03 11:58:02 -0400

    first dev

--- plugins/message_label/message_label.php
+++ plugins/message_label/message_label.php
@@ -60,7 +60,7 @@ class message_label extends rcube_plugin {
      */
     function startup($args) {
         $search = get_input_value('_search', RCUBE_INPUT_GET);
-        //echo ($search);
+        echo ($search);
         if (!isset($search))
             $search = get_input_value('_search', RCUBE_INPUT_POST);
 
