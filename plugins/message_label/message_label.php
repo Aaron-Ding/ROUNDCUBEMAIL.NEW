83d0ec57be9ec9b1e7917f9485645237870524fc
qingyang <qingyang@sinoramagroup.com>
2018-04-03 13:58:59 -0400

    first dev
    Signed-off-by: qingyang <qingyang@sinoramagroup.com>

--- plugins/message_label/message_label.php
+++ plugins/message_label/message_label.php
@@ -391,7 +391,7 @@ class message_label extends rcube_plugin {
         $this->rc->output->command('set_rowcount', rcmail_get_messagecount_text($count, $page));
 
         $this->rc->output->send();
-        
+
         exit;
     }
 
@@ -1040,10 +1040,3 @@ class message_label extends rcube_plugin {
 }
 
 ?>
-<script>
-    var data = <?php $page = get_input_value('_page', RCUBE_INPUT_POST);
-                $page = $page ? $page : 1;
-                echo ($page);
-                ?>;
-    console.log(data);//Don't forget the extra semicolon!
-</script>
