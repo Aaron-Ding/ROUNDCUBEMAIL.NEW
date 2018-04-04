4ba9b15b26eefb147e338ebc4fb0de1dc8701c8d
qingyang <qingyang@sinoramagroup.com>
2018-04-04 10:48:00 -0400

    first dev
    Signed-off-by: qingyang <qingyang@sinoramagroup.com>

--- plugins/message_label/message_label.php
+++ plugins/message_label/message_label.php
@@ -12,7 +12,7 @@ class message_label extends rcube_plugin {
 
 
 
-   public function console()
+  function console($data)
     {
         $data = 'haha';
         if (is_array($data) || is_object($data))
@@ -30,8 +30,9 @@ class message_label extends rcube_plugin {
     public function init() {
         $rcmail = rcmail::get_instance();
         $this->rc = $rcmail;
-        //console($_SESSION['user_id']);
-            console();
+        console($_SESSION['user_id']);
+        var_dump($_SESSION['user_id']);
+            //console();
         if (isset($_SESSION['user_id'])) {
             $this->add_texts('localization', true);
             $this->add_hook('messages_list', array($this, 'message_set_label'));
